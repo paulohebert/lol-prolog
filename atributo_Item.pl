d443a4ffe88712e7e95a34297cf858a5b843e09d
paulohebert <paulo.hebert@discente.univasf.edu.br>
2025-05-28 20:09:57 -0300

    fix: prolog syntax

---
+++ atributo_Item.pl
@@ -0,0 +1,172 @@
+atributo('Tomo amplificador', ap).
+atributo('Espada G.p.C', ad).
+atributo('Varinha explosiva', ap).
+atributo('Manto da Agilidade', ad).
+atributo('Manto de armadura', tank).
+atributo('Adaga', ad).
+atributo('Lágrima da Deusa', ap).
+atributo('Amuleto da Fada', ap).
+atributo('Cristal de Ruby', tank).
+atributo('Cristal de Ruby', tank).
+atributo('Cristal de Safira', ap).
+atributo('Lacre Sombrio', ap).
+atributo('Espada Longa', ad).
+atributo('Manto Anula-Magia', tank).
+atributo('Particula Brilhante', ap).
+atributo('Pérola do Rejuvenescimento', tank).
+atributo('Picareta', ad).
+atributo('Aljava Vespertina', ad).
+atributo('Alternador Hextec', ap).
+atributo('Arco Recurvo', ad).
+atributo('Armaguarda da Caçadora', ap).
+atributo('Armaguarda da Caçadora', tank).
+atributo('Bandana de mercúrio', tank).
+atributo('Bandeira Verdejante', ap).
+atributo('Bandeira Verdejante', tank).
+atributo('Brasa de Bami', tank).
+atributo('Braçadeira Cristalina', tank).
+atributo('Broquel Glacial', tank).
+atributo('Capa Megatron', tank).
+atributo('Capítulo Perdido', ap).
+atributo('Capuz do espectro',tank).
+atributo('Carapaça do Vigia', tank).
+atributo('Catalisador das eras', ap).
+atributo('Cetro Vampírico', ad).
+atributo('Chamado do Carrasco', ad).
+atributo('Cintilação Etérea', ap).
+atributo('Cinto Gigante', tank).
+atributo('Colete Espinhoso', tank).
+atributo('Cota de Malha', tank).
+atributo('Couraça Lunar Alada', tank).
+atributo('Ídolo Proibido', ap).
+atributo('Estilingue do Patrulheiro', ad).
+atributo('Retriz', ad).
+atributo('Gema Ardente', tank).
+atributo('Orbe do Oblivio', ap).
+atributo('Codex Demoníaco', ap).
+atributo('Fulgor', ad).
+atributo('Espelho de Bandópolis', ap).
+atributo('Berrante do Guardião', tank).
+atributo('Orbe do Guardião', ap).
+atributo('Lâmina do Guardião', ad).
+atributo('Martelo do Guardião', tank).
+atributo('Martelo do Guardião', ad).
+atributo('Punhal Serrilhado', ad).
+atributo('Martelo de Guerra de Caulfield', ad).
+atributo('Sigilo de aço', ad).
+atributo('Sigilo de aço', tank).
+atributo('Fago', ad).
+atributo('Êgide da Legião', tank).
+atributo('Joia da Ruina', ap).
+atributo('Tunelizador', ad).
+atributo('Bastão Desnecessariamente Grande', ap).
+atributo('Machado Termestre', ad).
+atributo('Tiamat', ad).
+atributo('Zelo', ad).
+atributo('Máscara Assustadora', ap).
+atributo('Hexdrinker', ad).
+atributo('Brutalizador', ad).
+atributo('Último Susurro', ad).
+atributo('Ladrão de almas de Mejai', ap).
+atributo('Hino Bélico de Shurelya', suport).
+atributo('Converjência de Zeke', tank).
+atributo('Medalão dos Solari de Ferro', suport).
+atributo('Turíbulo Ardente', suport).
+atributo('Regenador de Pedra Lunar', suport).
+atributo('Ecos de Helia', suport).
+atributo('Mandato Imperial', suport).
+atributo('Cajado Aquafluxo', suport).
+atributo('Redenção', suport).
+atributo('Juramento do Cavalheiro', suport).
+atributo('Juramento do Cavalheiro', tank).
+atributo('Benção de Mikael', suport).
+atributo('Desbravador', tank).
+atributo('Aproximação Invernal', tank).
+atributo('Armadura de espinhos', tank).
+atributo('Coração Congelado', tank).
+atributo('Glaive Sombria', ad).
+atributo('Auronúcleo', ap).
+atributo('Presa da Serpente', ad).
+atributo('Cetro de Cristal de Rylai', ap).
+atributo('Bastão das Eras', ap).
+atributo('Dançarina Fantasma', ad).
+atributo('Furacão de Runaan', ad).
+atributo('Canhão Fumegante', ad).
+atributo('Explocinturão Hextec', ap).
+atributo('Adaga Oscilante Navori', ad).
+atributo('Máscara Abissal', tank).
+atributo('Semblante Espiritual', tank).
+atributo('Égide de Fogo Solar', tank).
+atributo('Faca de Statikk', ad).
+atributo('Malevolência', ap).
+atributo('Presságio de Randuin', tank).
+atributo('Oportunidade', ad).
+atributo('Companheiro de Luden', ap).
+atributo('Desespero Eterno', tank).
+atributo('Tocha de Chamas Negras', ap).
+atributo('Limite da Razão', ad).
+atributo('Lâmina Fantasma de Youmuu', ad).
+atributo('Força da Natureza', tank).
+atributo('Ápice da Tempestade', ap).
+atributo('Resplendor Vazio', tank).
+atributo('Morellonomicon', ap).
+atributo('Rookern Lampurico', tank).
+atributo('Cajado do Arcanjo', ap).
+atributo('Manamune', ad).
+atributo('Perdição de Lich', ap).
+atributo('Dente de Nashor', ap).
+atributo('Colherdor de Essência', ad).
+atributo('Couraça do Defunto', tank).
+atributo('Foco do Horizonte', ap).
+atributo('Manopla dos Glacinatas', tank).
+atributo('Eclipse', ad).
+atributo('Maldição Sanguinária', ap).
+atributo('Húbris', ad).
+atributo('Flechatroz de Yun Tal', ad).
+atributo('Cutelo Negro', ad).
+atributo('Hexoplaca Experimental', tank).
+atributo('Coração de Aço', tank).
+atributo('Véu da Banshee', ap).
+atributo('Lâmina da Fúria de Guinsoo', ad).
+atributo('Lâmina da Fúria de Guinsoo', ap).
+atributo('Cajado do Vazio', ap).
+atributo('Criptoflora', ap).
+atributo('Quebracascos', ad).
+atributo('Terminus', ad).
+atributo('Limiar da Noite', ad).
+atributo('Ímpeto Cósmico', ap).
+atributo('Tormento de Liandry', ap).
+atributo('Arco-escudo Imortal', ad).
+atributo('A Coletora', ad).
+atributo('Rancor de Serylda', ad).
+atributo('Arco de Axioma', ad).
+atributo('Cicloespada Voltaica', ad).
+atributo('Lembranças do Lorde Dominik', ad).
+atributo('Armadura de Warmog', tank).
+atributo('Mandíbula de Malmórtius', ad).
+atributo('Lança de Shojin', ad).
+atributo('Criafendas', ap).
+atributo('Serraespada Quimiopunk', ad).
+atributo('Céu Dividido', ad).
+atributo('Mata-Cráquens', ad).
+atributo('Anjo Guardião', ad).
+atributo('Sinal de Sterak', tank).
+atributo('Sinal de Sterak', ad).
+atributo('Cimitarra Mercurial', ad).
+atributo('Espada do Rei Destruído', ad).
+atributo('Chama Sombria', ap).
+atributo('JakSho o Inconstante', tank).
+atributo('Hidra Profana', ad).
+atributo('Ampulheta de Zhonyas', ap).
+atributo('Armadura Sangrenta do Suserano', ad).
+atributo('Armadura Sangrenta do Suserano', ad).
+atributo('Lembrete Mortal', ad).
+atributo('Hidra Raivosa', ad).
+atributo('Hidra Titânica', tank).
+atributo('Hidra Titânica', ad).
+atributo('Dança da Morte', ad).
+atributo('Quebrapassos', ad).
+atributo('Força da Trindade', ad).
+atributo('Sedenta por Sangue', ad).
+atributo('Gume do infinito', ad).
+atributo('Capuz da Morte de Rabadon', ap).
---
+++ range_item.pl
@@ -0,0 +1,251 @@
+range_item('Tomo amplificador', ranged).
+range_item('Tomo amplificador', melee).
+range_item('Espada G.p.C', ranged).
+range_item('Espada G.p.C', melee).
+range_item('Varinha explosiva', ranged).
+range_item('Varinha explosiva', melee).
+range_item('Manto da Agilidade', ranged).
+range_item('Manto da Agilidade', melee).
+range_item('Manto de armadura', melee).
+range_item('Adaga', ranged).
+range_item('Adaga', melee).
+range_item('Lágrima da Deusa', ranged).
+range_item('Amuleto da Fada', ranged).
+range_item('Cristal de Ruby', melee).
+range_item('Cristal de Ruby', melee).
+range_item('Cristal de Safira', ranged).
+range_item('Lacre Sombrio', ranged).
+range_item('Espada Longa', ranged).
+range_item('Espada Longa', melee).
+range_item('Manto Anula-Magia', melee).
+range_item('Particula Brilhante', ranged).
+range_item('Particula Brilhante', melee).
+range_item('Pérola do Rejuvenescimento', melee).
+range_item('Pérola do Rejuvenescimento', ranged).
+range_item('Picareta', ranged).
+range_item('Picareta', melee).
+range_item('Aljava Vespertina', ranged).
+range_item('Aljava Vespertina', melee).
+range_item('Alternador Hextec', ranged).
+range_item('Alternador Hextec', melee).
+range_item('Arco Recurvo', ranged).
+range_item('Arco Recurvo', melee).
+range_item('Armaguarda da Caçadora', ranged).
+range_item('Armaguarda da Caçadora', melee).
+range_item('Bandana de mercúrio', melee).
+range_item('Bandana de mercúrio', ranged).
+range_item('Bandeira Verdejante', ranged).
+range_item('Bandeira Verdejante', melee).
+range_item('Brasa de Bami', melee).
+range_item('Brasa de Bami', ranged).
+range_item('Braçadeira Cristalina', melee).
+range_item('Broquel Glacial', melee).
+range_item('Capa Megatron', melee).
+range_item('Capítulo Perdido', ranged).
+range_item('Capítulo Perdido', melee).
+range_item('Capuz do espectro', melee).
+range_item('Carapaça do Vigia', melee).
+range_item('Catalisador das eras', ranged).
+range_item('Catalisador das eras', melee).
+range_item('Cetro Vampírico', melee).
+range_item('Cetro Vampírico', ranged).
+range_item('Chamado do Carrasco', ranged).
+range_item('Chamado do Carrasco', melee).
+range_item('Cintilação Etérea', ranged).
+range_item('Cintilação Etérea', melee).
+range_item('Cinto Gigante', melee).
+range_item('Colete Espinhoso', melee).
+range_item('Cota de Malha', melee).
+range_item('Couraça Lunar Alada', melee).
+range_item('Ídolo Proibido', ranged).
+range_item('Ídolo Proibido', melee).
+range_item('Estilingue do Patrulheiro', ranged).
+range_item('Retriz', ranged).
+range_item('Retriz', melee).
+range_item('Gema Ardente', melee).
+range_item('Orbe do Oblivio', ranged).
+range_item('Orbe do Oblivio', melee).
+range_item('Codex Demoníaco', ranged).
+range_item('Codex Demoníaco', melee).
+range_item('Fulgor', ranged).
+range_item('Fulgor', melee).
+range_item('Espelho de Bandópolis', ranged).
+range_item('Berrante do Guardião', melee).
+range_item('Orbe do Guardião', ranged).
+range_item('Orbe do Guardião', melee).
+range_item('Lâmina do Guardião', ranged).
+range_item('Lâmina do Guardião', melee).
+range_item('Martelo do Guardião', melee).
+range_item('Punhal Serrilhado', ranged).
+range_item('Punhal Serrilhado', melee).
+range_item('Martelo de Guerra de Caulfield', ranged).
+range_item('Martelo de Guerra de Caulfield', melee).
+range_item('Sigilo de aço', ranged).
+range_item('Sigilo de aço', melee).
+range_item('Fago', ranged).
+range_item('Fago', melee).
+range_item('Êgide da Legião', melee).
+range_item('Joia da Ruina', ranged).
+range_item('Joia da Ruina', melee).
+range_item('Tunelizador', ranged).
+range_item('Tunelizador', melee).
+range_item('Bastão Desnecessariamente Grande', ranged).
+range_item('Bastão Desnecessariamente Grande', melee).
+range_item('Machado Termestre', ranged).
+range_item('Machado Termestre', melee).
+range_item('Tiamat', melee).
+range_item('Zelo', ranged).
+range_item('Zelo', melee).
+range_item('Máscara Assustadora', ranged).
+range_item('Máscara Assustadora', melee).
+range_item('Hexdrinker', ranged).
+range_item('Hexdrinker', melee).
+range_item('Brutalizador', ranged).
+range_item('Brutalizador', melee).
+range_item('Último Susurro', ranged).
+range_item('Último Susurro', melee).
+range_item('Ladrão de almas de Mejai', ranged).
+range_item('Ladrão de almas de Mejai', melee).
+range_item('Hino Bélico de Shurelya', ranged).
+range_item('Hino Bélico de Shurelya', melee).
+range_item('Converjência de Zeke', melee).
+range_item('Medalão dos Solari de Ferro', ranged).
+range_item('Medalão dos Solari de Ferro', melee).
+range_item('Turíbulo Ardente', ranged).
+range_item('Regenador de Pedra Lunar', ranged).
+range_item('Regenador de Pedra Lunar', melee).
+range_item('Ecos de Helia', ranged).
+range_item('Mandato Imperial', ranged).
+range_item('Cajado Aquafluxo', ranged).
+range_item('Redenção', ranged).
+range_item('Redenção', melee).
+range_item('Juramento do Cavalheiro', melee).
+range_item('Benção de Mikael', ranged).
+range_item('Desbravador', melee).
+range_item('Aproximação Invernal', melee).
+range_item('Armadura de espinhos', melee).
+range_item('Coração Congelado', melee).
+range_item('Glaive Sombria', melee).
+range_item('Glaive Sombria', ranged).
+range_item('Auronúcleo', ranged).
+range_item('Presa da Serpente', ranged).
+range_item('Presa da Serpente', melee).
+range_item('Cetro de Cristal de Rylai', ranged).
+range_item('Bastão das Eras', ranged).
+range_item('Dançarina Fantasma', ranged).
+range_item('Dançarina Fantasma', melee).
+range_item('Furacão de Runaan', ranged).
+range_item('Canhão Fumegante', ranged).
+range_item('Explocinturão Hextec', ranged).
+range_item('Explocinturão Hextec', melee).
+range_item('Adaga Oscilante Navori', ranged).
+range_item('Adaga Oscilante Navori', melee).
+range_item('Máscara Abissal', melee).
+range_item('Semblante Espiritual', melee).
+range_item('Égide de Fogo Solar', melee).
+range_item('Faca de Statikk', ranged).
+range_item('Faca de Statikk', melee).
+range_item('Malevolência', ranged).
+range_item('Presságio de Randuin', melee).
+range_item('Oportunidade', melee).
+range_item('Companheiro de Luden', ranged).
+range_item('Desespero Eterno', melee).
+range_item('Tocha de Chamas Negras', ranged).
+range_item('Tocha de Chamas Negras', melee).
+range_item('Limite da Razão', ranged).
+range_item('Limite da Razão', melee).
+range_item('Lâmina Fantasma de Youmuu', ranged).
+range_item('Lâmina Fantasma de Youmuu', melee).
+range_item('Força da Natureza', melee).
+range_item('Ápice da Tempestade', ranged).
+range_item('Resplendor Vazio', melee).
+range_item('Morellonomicon', ranged).
+range_item('Morellonomicon', melee).
+range_item('Rookern Lampurico', melee).
+range_item('Cajado do Arcanjo', ranged).
+range_item('Manamune', ad).
+range_item('Perdição de Lich', ranged).
+range_item('Perdição de Lich', melee).
+range_item('Dente de Nashor', ranged).
+range_item('Dente de Nashor', melee).
+range_item('Colherdor de Essência', ranged).
+range_item('Colherdor de Essência', melee).
+range_item('Couraça do Defunto', melee).
+range_item('Foco do Horizonte', ranged).
+range_item('Manopla dos Glacinatas', melee).
+range_item('Eclipse', melee).
+range_item('Eclipse', ranged).
+range_item('Maldição Sanguinária', ranged).
+range_item('Húbris', ranged).
+range_item('Húbris', melee).
+range_item('Flechatroz de Yun Tal', ranged).
+range_item('Flechatroz de Yun Tal', melee).
+range_item('Cutelo Negro', melee).
+range_item('Hexoplaca Experimental', melee).
+range_item('Coração de Aço', melee).
+range_item('Véu da Banshee', ranged).
+range_item('Véu da Banshee', melee).
+range_item('Lâmina da Fúria de Guinsoo', melee).
+range_item('Lâmina da Fúria de Guinsoo', ranged).
+range_item('Cajado do Vazio', ranged).
+range_item('Cajado do Vazio', melee).
+range_item('Criptoflora', ranged).
+range_item('Quebracascos', melee).
+range_item('Terminus', ranged).
+range_item('Terminus', melee).
+range_item('Limiar da Noite', ranged).
+range_item('Limiar da Noite', melee).
+range_item('Ímpeto Cósmico', ranged).
+range_item('Ímpeto Cósmico', melee).
+range_item('Tormento de Liandry', ranged).
+range_item('Tormento de Liandry', melee).
+range_item('Arco-escudo Imortal', ranged).
+range_item('Arco-escudo Imortal', meele).
+range_item('A Coletora', ranged).
+range_item('A Coletora', melee).
+range_item('Rancor de Serylda', ranged).
+range_item('Rancor de Serylda', melee).
+range_item('Arco de Axioma', ranged).
+range_item('Arco de Axioma', melee).
+range_item('Cicloespada Voltaica', ranged).
+range_item('Cicloespada Voltaica', melee).
+range_item('Lembranças do Lorde Dominik', ranged).
+range_item('Lembranças do Lorde Dominik', melee).
+range_item('Armadura de Warmog', melee).
+range_item('Mandíbula de Malmórtius', ranged).
+range_item('Mandíbula de Malmórtius', melee).
+range_item('Lança de Shojin', ranged).
+range_item('Lança de Shojin', melee).
+range_item('Criafendas', ranged).
+range_item('Criafendas', melee).
+range_item('Serraespada Quimiopunk', melee).
+range_item('Céu Dividido', melee).
+range_item('Mata-Cráquens', ranged).
+range_item('Mata-Cráquens', melee).
+range_item('Anjo Guardião', ranged).
+range_item('Anjo Guardião', melee).
+range_item('Sinal de Sterak', melee).
+range_item('Cimitarra Mercurial', melee).
+range_item('Espada do Rei Destruído', ranged).
+range_item('Espada do Rei Destruído', melee).
+range_item('Chama Sombria', ranged).
+range_item('Chama Sombria', melee).
+range_item('JakSho o Inconstante', melee).
+range_item('Hidra Profana', melee).
+range_item('Ampulheta de Zhonyas', ranged).
+range_item('Ampulheta de Zhonyas', melee).
+range_item('Armadura Sangrenta do Suserano', melee).
+range_item('Lembrete Mortal', ranged).
+range_item('Lembrete Mortal', melee).
+range_item('Hidra Raivosa', melee).
+range_item('Hidra Titânica', melee).
+range_item('Dança da Morte', melee).
+range_item('Quebrapassos', melee).
+range_item('Força da Trindade', ranged).
+range_item('Força da Trindade', melee).
+range_item('Sedenta por Sangue', ranged).
+range_item('Sedenta por Sangue', melee).
+range_item('Gume do infinito', ranged).
+range_item('Gume do infinito', melee).
+range_item('Capuz da Morte de Rabadon', ranged).
+range_item('Capuz da Morte de Rabadon', melee).
