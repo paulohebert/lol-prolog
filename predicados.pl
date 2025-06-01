8d6d3f0dd6d9e8f931ae24a662ab04ae7b59e007
paulohebert <paulo.hebert@discente.univasf.edu.br>
2025-06-01 19:09:09 -0300

    feat: add predicados

---
+++ predicados.pl
@@ -0,0 +1,26 @@
+:- ['atributo_Item.pl' ,'categoria.pl', 'classe.pl', 'range_item.pl', 'range_type.pl', 'receita.pl', 'usa_atributo.pl'].
+
+campeao(Nome, Classe, Ranged) :- classe(Nome, Classe), ranged_type(Nome, Ranged).
+
+item(ItemContruido, TipoItemReceita, TipoItemContruido) :-
+    categoria(ItemReceita, TipoItemReceita),
+    categoria(ItemContruido, TipoItemContruido),
+    receita(ItemContruido, ItemReceita).
+
+recomendado(Item, Campeao) :-
+    distinct(Atributo, (
+        classe(Campeao, Classe),
+        usa_atributo(Classe, Atributo)
+    )),
+    atributo(Item, Atributo),
+    ranged_type(Campeao, Ranged),
+    range_item(Item, Ranged).
+
+build(Item, Constroi) :- receita(Constroi, Item).
+build(Item, Constroi) :- distinct(Constroi, (receita(Constroi, X), build(Item, X))).
+
+pode_usar(Atributo, Campeao) :- 
+    distinct(Campeao, (
+        classe(Campeao, Classe),
+        usa_atributo(Classe, Atributo)
+    )).
