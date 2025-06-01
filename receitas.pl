b29107c7560ab0450a5cc0f3f2e7efb92b23fde3
paulohebert <paulo.hebert@discente.univasf.edu.br>
2025-05-31 22:46:39 -0300

    feat: add recipe item

---
+++ receitas.pl
@@ -0,0 +1,301 @@
+receita('A Coletora', 'Capa da Agilidade').
+receita('A Coletora', 'Picareta').
+receita('A Coletora', 'Punhal Serrilhado').
+receita('Adaga Oscilante Navori', 'Adaga').
+receita('Adaga Oscilante Navori', 'Zelo').
+receita('Aljava Vespertina', 'Capa da Agilidade').
+receita('Aljava Vespertina', 'Espada Longa').
+receita('Alternador Hextec', 'Tomo amplificador').
+receita('Ampulheta de Zhonya', 'Armaguarda da Caçadora').
+receita('Ampulheta de Zhonya', 'Bastão Desnecessariamente Grande').
+receita('Anjo Guardião', 'Espada G.p.C').
+receita('Anjo Guardião', 'Sigilo de aço').
+receita('Aproximação Invernal', 'Cinto do Gigante').
+receita('Aproximação Invernal', 'Gema Ardente').
+receita('Aproximação Invernal', 'Lágrima da Deusa').
+receita('Arco Recurvo', 'Adaga').
+receita('Arco do Axioma', 'Brutalizador').
+receita('Arco do Axioma', 'Martelo de Guerra de Caulfield').
+receita('Arcoescudo Imortal', 'Aljava Vespertina').
+receita('Arcoescudo Imortal', 'Picareta').
+receita('Armadura Sangrenta do Suserano', 'Tunelizador').
+receita('Armadura de espinhos', 'Colete Espinhoso').
+receita('Armadura de espinhos', 'Cota de Malha').
+receita('Armadura de espinhos', 'Cristal de Ruby').
+receita('Armadura de Warmog', 'Braçadeira Cristalina').
+receita('Armadura de Warmog', 'Cinto do Gigante').
+receita('Armaguarda da Caçadora', 'Couraça de Pano').
+receita('Armaguarda da Caçadora', 'Tomo amplificador').
+receita('Auronúcleo', 'Varinha explosiva').
+receita('Auronúcleo', 'Ídolo Proibido').
+receita('Bandana de mercúrio', 'Manto Anula-Magia').
+receita('Barreira Verdejante', 'Manto Anula-Magia').
+receita('Barreira Verdejante', 'Tomo amplificador').
+receita('Bastão das Eras', 'Catalisador das eras').
+receita('Bastão das Eras', 'Varinha explosiva').
+receita('Brasa de Bami', 'Cristal de Ruby').
+receita('Brasa de Bami', 'Particula Brilhante').
+receita('Braçadeira Cristalina', 'Cristal de Ruby').
+receita('Braçadeira Cristalina', 'Pérola do Rejuvenescimento').
+receita('Broquel Glacial', 'Couraça de Pano').
+receita('Broquel Glacial', 'Cristal de Safira').
+receita('Broquel Glacial', 'Particula Brilhante').
+receita('Brutalizador', 'Particula Brilhante').
+receita('Brutalizador', 'Picareta').
+receita('Benção de Mikael', 'Cristal de Ruby').
+receita('Benção de Mikael', 'Gema Ardente').
+receita('Benção de Mikael', 'Ídolo Proibido').
+receita('Cajado Aquafluxo', 'Codex Demoníaco').
+receita('Cajado Aquafluxo', 'Tomo amplificador').
+receita('Cajado Aquafluxo', 'Ídolo Proibido').
+receita('Cajado do Vazio', 'Joia da Ruina').
+receita('Cajado do Vazio', 'Varinha explosiva').
+receita('Canhão Fumegante', 'Estilingue do Patrulheiro').
+receita('Canhão Fumegante', 'Zelo').
+receita('Capa Negatron', 'Manto Anula-Magia').
+receita('Capuz da Morte de Rabadon', 'Bastão Desnecessariamente Grande').
+receita('Capuz do espectro', 'Cristal de Ruby').
+receita('Capuz do espectro', 'Manto Anula-Magia').
+receita('Capuz do espectro', 'Pérola do Rejuvenescimento').
+receita('Capítulo Perdido', 'Cristal de Safira').
+receita('Capítulo Perdido', 'Particula Brilhante').
+receita('Capítulo Perdido', 'Tomo amplificador').
+receita('Carapaça do Vigia', 'Couraça de Pano').
+receita('Catalisador das eras', 'Cristal de Ruby').
+receita('Catalisador das eras', 'Cristal de Safira').
+receita('Cetro Vampírico', 'Espada Longa').
+receita('Cetro de Cristal de Rylai', 'Cinto do Gigante').
+receita('Cetro de Cristal de Rylai', 'Tomo amplificador').
+receita('Cetro de Cristal de Rylai', 'Varinha explosiva').
+receita('Chama Sombria', 'Alternador Hextec').
+receita('Chama Sombria', 'Bastão Desnecessariamente Grande').
+receita('Chamado do Carrasco', 'Espada Longa').
+receita('Cicloespada Voltaica', 'Brutalizador').
+receita('Cicloespada Voltaica', 'Espada Longa').
+receita('Cimitarra Mercurial', 'Bandana de mercúrio').
+receita('Cimitarra Mercurial', 'Cetro Vampírico').
+receita('Cimitarra Mercurial', 'Picareta').
+receita('Cintilação Etérea', 'Tomo amplificador').
+receita('Cinto do Gigante', 'Cristal de Ruby').
+receita('Colete Espinhoso', 'Couraça de Pano').
+receita('Colhedor de Essência', 'Capa da Agilidade').
+receita('Colhedor de Essência', 'Martelo de Guerra de Caulfield').
+receita('Colhedor de Essência', 'Picareta').
+receita('Companheiro de Luden', 'Alternador Hextec').
+receita('Companheiro de Luden', 'Capítulo Perdido').
+receita('Convergência de Zeke', 'Couraça de Pano').
+receita('Convergência de Zeke', 'Cristal de Ruby').
+receita('Convergência de Zeke', 'Gema Ardente').
+receita('Convergência de Zeke', 'Manto Anula-Magia').
+receita('Convergência de Zeke', 'Êgide da Legião').
+receita('Coração Congelado', 'Broquel Glacial').
+receita('Coração Congelado', 'Carapaça do Vigia').
+receita('Coração de Aço', 'Braçadeira Cristalina').
+receita('Coração de Aço', 'Cinto do Gigante').
+receita('Cota de Malha', 'Couraça de Pano').
+receita('Couraça Lunar Alada', 'Cristal de Ruby').
+receita('Couraça do Defunto', 'Cota de Malha').
+receita('Couraça do Defunto', 'Couraça Lunar Alada').
+receita('Couraça do Defunto', 'Cristal de Ruby').
+receita('Criafendas', 'Codex Demoníaco').
+receita('Criafendas', 'Máscara Assustadora').
+receita('Criptoflora', 'Codex Demoníaco').
+receita('Criptoflora', 'Joia da Ruina').
+receita('Cutelo Negro', 'Fago').
+receita('Cutelo Negro', 'Gema Ardente').
+receita('Cutelo Negro', 'Picareta').
+receita('Céu Dividido', 'Cristal de Ruby').
+receita('Céu Dividido', 'Martelo de Guerra de Caulfield').
+receita('Céu Dividido', 'Tunelizador').
+receita('Codex Demoníaco', 'Particula Brilhante').
+receita('Codex Demoníaco', 'Tomo amplificador').
+receita('Dança da Morte', 'Martelo de Guerra de Caulfield').
+receita('Dança da Morte', 'Picareta').
+receita('Dança da Morte', 'Sigilo de aço').
+receita('Dançarina Fantasma', 'Adaga').
+receita('Dançarina Fantasma', 'Zelo').
+receita('Dente de Nashor', 'Arco Recurvo').
+receita('Dente de Nashor', 'Codex Demoníaco').
+receita('Dente de Nashor', 'Varinha explosiva').
+receita('Desbravador', 'Cota de Malha').
+receita('Desbravador', 'Couraça Lunar Alada').
+receita('Desespero Eterno', 'Cinto do Gigante').
+receita('Desespero Eterno', 'Êgide da Legião').
+receita('Eclipse', 'Espada Longa').
+receita('Eclipse', 'Martelo de Guerra de Caulfield').
+receita('Eclipse', 'Picareta').
+receita('Ecos de Helia', 'Espelho de Bandópolis').
+receita('Ecos de Helia', 'Gema Ardente').
+receita('Espada do Rei Destruído', 'Arco Recurvo').
+receita('Espada do Rei Destruído', 'Cetro Vampírico').
+receita('Espada do Rei Destruído', 'Picareta').
+receita('Espelho de Bandópolis', 'Amuleto da Fada').
+receita('Espelho de Bandópolis', 'Particula Brilhante').
+receita('Espelho de Bandópolis', 'Tomo amplificador').
+receita('Estilingue do Patrulheiro', 'Adaga').
+receita('Explocinturão Hextec', 'Alternador Hextec').
+receita('Explocinturão Hextec', 'Cristal de Ruby').
+receita('Explocinturão Hextec', 'Codex Demoníaco').
+receita('Faca de Statikk', 'Estilingue do Patrulheiro').
+receita('Faca de Statikk', 'Picareta').
+receita('Faca de Statikk', 'Retriz').
+receita('Fago', 'Cristal de Ruby').
+receita('Fago', 'Espada Longa').
+receita('Flechatroz de Yun Tal', 'Espada G.p.C').
+receita('Flechatroz de Yun Tal', 'Espada Longa').
+receita('Flechatroz de Yun Tal', 'Estilingue do Patrulheiro').
+receita('Foco do Horizonte', 'Codex Demoníaco').
+receita('Foco do Horizonte', 'Varinha explosiva').
+receita('Força da Trindade', 'Fago').
+receita('Força da Trindade', 'Fulgor').
+receita('Força da Trindade', 'Machado Termestre').
+receita('Fulgor', 'Particula Brilhante').
+receita('Furacão de Runaan', 'Estilingue do Patrulheiro').
+receita('Furacão de Runaan', 'Zelo').
+receita('Gema Ardente', 'Cristal de Ruby').
+receita('Gema Ardente', 'Particula Brilhante').
+receita('Glaive Sombria', 'Martelo de Guerra de Caulfield').
+receita('Glaive Sombria', 'Punhal Serrilhado').
+receita('Gume do infinito', 'Capa da Agilidade').
+receita('Gume do infinito', 'Espada G.p.C').
+receita('Gume do infinito', 'Picareta').
+receita('Hexdrinker', 'Espada Longa').
+receita('Hexdrinker', 'Manto Anula-Magia').
+receita('Hexoplaca Experimental', 'Adaga').
+receita('Hexoplaca Experimental', 'Fago').
+receita('Hexoplaca Experimental', 'Tunelizador').
+receita('Hidra Profana', 'Brutalizador').
+receita('Hidra Profana', 'Tiamat').
+receita('Hidra Raivosa', 'Cetro Vampírico').
+receita('Hidra Raivosa', 'Martelo de Guerra de Caulfield').
+receita('Hidra Raivosa', 'Tiamat').
+receita('Hidra Titânica', 'Cinto do Gigante').
+receita('Hidra Titânica', 'Tiamat').
+receita('Hidra Titânica', 'Tunelizador').
+receita('Hino Bélico de Shurelya', 'Cintilação Etérea').
+receita('Hino Bélico de Shurelya', 'Espelho de Bandópolis').
+receita('Húbris', 'Martelo de Guerra de Caulfield').
+receita('Húbris', 'Punhal Serrilhado').
+receita('JakSho o Inconstante', 'Capa Negatron').
+receita('JakSho o Inconstante', 'Cinto do Gigante').
+receita('JakSho o Inconstante', 'Cota de Malha').
+receita('Joia da Ruina', 'Tomo amplificador').
+receita('Juramento do Cavaleiro', 'Cota de Malha').
+receita('Juramento do Cavaleiro', 'Gema Ardente').
+receita('Juramento do Cavaleiro', 'Pérola do Rejuvenescimento').
+receita('Ladrão de almas de Mejai', 'Lacre Sombrio').
+receita('Lança de Shojin', 'Cristal de Ruby').
+receita('Lança de Shojin', 'Picareta').
+receita('Lança de Shojin', 'Tunelizador').
+receita('Lembranças do Lorde Dominik', 'Aljava Vespertina').
+receita('Lembranças do Lorde Dominik', 'Último Sussurro').
+receita('Lembrete Mortal', 'Capa da Agilidade').
+receita('Lembrete Mortal', 'Chamado do Carrasco').
+receita('Lembrete Mortal', 'Último Sussurro').
+receita('Limiar da Noite', 'Punhal Serrilhado').
+receita('Limiar da Noite', 'Tunelizador').
+receita('Limite da Razão', 'Arco Recurvo').
+receita('Limite da Razão', 'Capa Negatron').
+receita('Lâmina Fantasma de Youmuu', 'Espada Longa').
+receita('Lâmina Fantasma de Youmuu', 'Punhal Serrilhado').
+receita('Lâmina Fantasma de Youmuu', 'Retriz').
+receita('Lâmina da Fúria de Guinsoo', 'Arco Recurvo').
+receita('Lâmina da Fúria de Guinsoo', 'Picareta').
+receita('Lâmina da Fúria de Guinsoo', 'Tomo amplificador').
+receita('Machado Termestre', 'Adaga').
+receita('Machado Termestre', 'Espada Longa').
+receita('Maldição Sanguinária', 'Codex Demoníaco').
+receita('Maldição Sanguinária', 'Máscara Assustadora').
+receita('Malevolência', 'Capítulo Perdido').
+receita('Malevolência', 'Varinha explosiva').
+receita('Mandato Imperial', 'Codex Demoníaco').
+receita('Mandato Imperial', 'Espelho de Bandópolis').
+receita('Mandato Imperial', 'Tomo amplificador').
+receita('Mandíbula de Malmórtius', 'Hexdrinker').
+receita('Mandíbula de Malmórtius', 'Martelo de Guerra de Caulfield').
+receita('Manopla dos Glacinatas', 'Cota de Malha').
+receita('Manopla dos Glacinatas', 'Cristal de Ruby').
+receita('Manopla dos Glacinatas', 'Fulgor').
+receita('Martelo de Guerra de Caulfield', 'Espada Longa').
+receita('Martelo de Guerra de Caulfield', 'Particula Brilhante').
+receita('Mata-Cráquens', 'Arco Recurvo').
+receita('Mata-Cráquens', 'Machado Termestre').
+receita('Mata-Cráquens', 'Retriz').
+receita('Medalhão dos Solari de Ferro', 'Couraça de Pano').
+receita('Medalhão dos Solari de Ferro', 'Gema Ardente').
+receita('Medalhão dos Solari de Ferro', 'Manto Anula-Magia').
+receita('Morellonomicon', 'Gema Ardente').
+receita('Morellonomicon', 'Orbe do Oblivio').
+receita('Morellonomicon', 'Varinha explosiva').
+receita('Máscara Abissal', 'Capa Negatron').
+receita('Máscara Abissal', 'Gema Ardente').
+receita('Máscara Assustadora', 'Cristal de Ruby').
+receita('Máscara Assustadora', 'Tomo amplificador').
+receita('Oportunidade', 'Espada Longa').
+receita('Oportunidade', 'Picareta').
+receita('Oportunidade', 'Punhal Serrilhado').
+receita('Orbe do Oblivio', 'Tomo amplificador').
+receita('Perdição de Lich', 'Cintilação Etérea').
+receita('Perdição de Lich', 'Fulgor').
+receita('Perdição de Lich', 'Varinha explosiva').
+receita('Presa da Serpente', 'Picareta').
+receita('Presa da Serpente', 'Punhal Serrilhado').
+receita('Presságio de Randuin', 'Carapaça do Vigia').
+receita('Presságio de Randuin', 'Cinto do Gigante').
+receita('Punhal Serrilhado', 'Espada Longa').
+receita('Quebracascos', 'Couraça Lunar Alada').
+receita('Quebracascos', 'Picareta').
+receita('Quebracascos', 'Tunelizador').
+receita('Quebrapassos', 'Adaga').
+receita('Quebrapassos', 'Fago').
+receita('Quebrapassos', 'Tiamat').
+receita('Rancor de Serylda', 'Martelo de Guerra de Caulfield').
+receita('Rancor de Serylda', 'Último Sussurro').
+receita('Redenção', 'Cristal de Ruby').
+receita('Redenção', 'Gema Ardente').
+receita('Redenção', 'Ídolo Proibido').
+receita('Regenerador de Pedra Lunar', 'Cristal de Ruby').
+receita('Regenerador de Pedra Lunar', 'Espelho de Bandópolis').
+receita('Regenerador de Pedra Lunar', 'Gema Ardente').
+receita('Resplendor Vazio', 'Brasa de Bami').
+receita('Resplendor Vazio', 'Capuz do espectro').
+receita('Retriz', 'Espada Longa').
+receita('Rookern Lamúrico', 'Capa Negatron').
+receita('Rookern Lamúrico', 'Capuz do espectro').
+receita('Sedenta por Sangue', 'Cetro Vampírico').
+receita('Sedenta por Sangue', 'Espada G.p.C').
+receita('Sedenta por Sangue', 'Picareta').
+receita('Semblante Espiritual', 'Capuz do espectro').
+receita('Semblante Espiritual', 'Gema Ardente').
+receita('Serrespada Quimiopunk', 'Chamado do Carrasco').
+receita('Serrespada Quimiopunk', 'Cinto do Gigante').
+receita('Serrespada Quimiopunk', 'Martelo de Guerra de Caulfield').
+receita('Sigilo de aço', 'Couraça de Pano').
+receita('Sigilo de aço', 'Espada Longa').
+receita('Sinal de Sterak', 'Cristal de Ruby').
+receita('Sinal de Sterak', 'Picareta').
+receita('Sinal de Sterak', 'Tunelizador').
+receita('Terminus', 'Arco Recurvo').
+receita('Terminus', 'Machado Termestre').
+receita('Tiamat', 'Espada Longa').
+receita('Tormento de Liandry', 'Máscara Assustadora').
+receita('Tunelizador', 'Cristal de Ruby').
+receita('Tunelizador', 'Espada Longa').
+receita('Turíbulo Ardente', 'Cintilação Etérea').
+receita('Turíbulo Ardente', 'Tomo amplificador').
+receita('Turíbulo Ardente', 'Ídolo Proibido').
+receita('Véu da Banshee', 'Barreira Verdejante').
+receita('Véu da Banshee', 'Bastão Desnecessariamente Grande').
+receita('Zelo', 'Adaga').
+receita('Zelo', 'Capa da Agilidade').
+receita('Ápice da Tempestade', 'Alternador Hextec').
+receita('Ápice da Tempestade', 'Cintilação Etérea').
+receita('Êgide da Legião', 'Couraça de Pano').
+receita('Êgide da Legião', 'Manto Anula-Magia').
+receita('Égide de Fogo Solar', 'Brasa de Bami').
+receita('Égide de Fogo Solar', 'Cota de Malha').
+receita('Ídolo Proibido', 'Amuleto da Fada').
+receita('Ímpeto Cósmico', 'Cintilação Etérea').
+receita('Ímpeto Cósmico', 'Codex Demoníaco').
+receita('Ímpeto Cósmico', 'Gema Ardente').
+receita('Último Sussurro', 'Espada Longa').
